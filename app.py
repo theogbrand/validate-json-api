1d311a422e050ff8cb0796bd17999a45de27687c
ogb <ongjjbrandon@gmail.com>
2024-04-20 08:52:23 +0800

    works

--- app.py
+++ app.py
@@ -428,43 +428,13 @@ Update text enclosed in <>. Be concise. Output only the json string without any 
 
     return {}
 
-# @app.route('/validate', methods=['POST'])
-@api.route('/validate')
+@api.route('/validate', methods=['POST'])
 def valid_json():
-  # data = request.get_json()
-  # system_prompt = data.get('system_prompt')
-  # user_prompt = data.get('user_prompt')
-  # output_format = data.get('output_format')
+  data = request.get_json()
+  system_prompt = data.get('system_prompt')
+  user_prompt = data.get('user_prompt')
+  output_format = data.get('output_format')
 
-  subQnPrompt = """You are an AI language model assistant. Your task is to generate Five
-    different versions of the given user question to retrieve relevant documents from a vector
-    database. By generating multiple perspectives on the user question, your goal is to help
-    the user overcome some of the limitations of the distance-based similarity search.
-    Provide these alternative questions seperated by newlines only.
-
-    For example:
-    User question: "What is the conclusion of the paper?"
-
-    Generated questions:
-    What is the main takeaway from the paper?
-    What are the key findings of the paper?
-    What is the summary of the paper?
-    What is the final thought of the paper?
-    What is the ending of the paper?
-    
-    Output format should be as follows:
-
-    'What is Bill Gates known for?'
-│   "Can you provide information about Bill Gates' background?"
-
-    And not this format:
-
-    '1. What is Bill Gates known for?'
-│   "2. Can you provide information about Bill Gates' background?"
-    """
-
-  return validate_json(system_prompt=subQnPrompt,
-            user_prompt="What is the conclusion of the paper?",
-            output_format = {'Generatd Questions': 'Five different versions of questions generated from user question, type: Array[str]',
-                                     'Total Number of Questions Generated': 'Number of Generated Questions, type: int'
-                                     })
+  return validate_json(system_prompt=system_prompt,
+            user_prompt=user_prompt,
+            output_format=output_format)
